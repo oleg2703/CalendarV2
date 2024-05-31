5e6443ec3d1d6bb350e18c04211f22f202dddbf0
oleg2703 <oleg2703lebid@gmail.com>
2024-05-31 15:01:52 +0300

    12

---
+++ Calendar/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const HomeScreen = () => {
+
+
+  return (
+    <View >
+      <Text >
+        Welcome to the Home Screen
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;

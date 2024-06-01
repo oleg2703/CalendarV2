5c79346174c544533613b0dd5a435d701ee6e29b
oleg2703 <oleg2703lebid@gmail.com>
2024-06-01 16:21:19 +0300

    -

--- Calendar/App.js
+++ Calendar/App.js
@@ -1,5 +1,7 @@
 import AppNavigator from "./navigation/AppNavigator";
-
+import registerNNPushToken from 'native-notify';
+import React, { useState, useEffect } from 'react';
 export default function App() {
+  registerNNPushToken(21616, 'WQRqRfdAqD9p8Wzjkww6LZ');
   return <AppNavigator/>;
 }

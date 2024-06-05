5446cb427c9277d591cd018ef27e37c46b098d29
oleg2703 <oleg2703lebid@gmail.com>
2024-06-05 20:36:21 +0300

    -

--- Calendar/screens/Schedule.js
+++ Calendar/screens/Schedule.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { View, Button, StyleSheet, Text, ScrollView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
+
+const renderSchedule = (schedule) => {
+  if (!schedule) {
+    return <Text>No schedule available</Text>;
+  }
+
+  const weeks = Object.keys(schedule);
+  return weeks.map((week) => (
+    <View key={week}>
+      <Text style={styles.weekTitle}>{week}</Text>
+      {Object.keys(schedule[week]).map((day) => (
+        <View key={day} style={styles.dayContainer}>
+          <Text style={styles.dayTitle}>{day}</Text>
+          {Object.keys(schedule[week][day]).map((subgroup) => (
+            <View key={subgroup} style={styles.subgroupContainer}>
+              <Text style={styles.subgroupTitle}>{subgroup}</Text>
+              {schedule[week][day][subgroup].map((lesson, index) => (
+                <View key={index} style={styles.lessonContainer}>
+                  <Text style={styles.lessonText}>
+                    {lesson.number}. {lesson.subject} ({lesson.type}) - {lesson.room} ({lesson.teacher})
+                  </Text>
+                </View>
+              ))}
+            </View>
+          ))}
+        </View>
+      ))}
+    </View>
+  ));
+};
 
 function Schedule() {
   const navigation = useNavigation();
   const route = useRoute();
-  const { injectedJavaScript, selectedLink } = route.params;
+  const { scheduleData, group } = route.params;
 
   return (
     <View style={{ flex: 1 }}>
       <View style={styles.buttonContainer}>
         <Button title="Назад до вибору розкладу" onPress={() => navigation.goBack()} />
       </View>
-      <WebView
-        source={{ uri: selectedLink }}
-        style={{ flex: 1 }}
-        injectedJavaScript={injectedJavaScript}
-        scalesPageToFit={true}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-      />
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.groupTitle}>Розклад для групи {group}</Text>
+        {scheduleData && scheduleData.schedule
+          ? renderSchedule(scheduleData.schedule)
+          : <Text>No schedule data available</Text>}
+      </ScrollView>
     </View>
   );
 }
@@ -28,7 +55,41 @@ function Schedule() {
 const styles = StyleSheet.create({
   buttonContainer: {
     height: "10%",
-    justifyContent:"flex-end" // Adjust this value as needed
+    justifyContent: "flex-end",
+  },
+  scrollView: {
+    flex: 1,
+    padding: 10,
+  },
+  groupTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  weekTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  dayContainer: {
+    marginTop: 5,
+  },
+  dayTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  subgroupContainer: {
+    marginLeft: 10,
+  },
+  subgroupTitle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  lessonContainer: {
+    marginLeft: 20,
+  },
+  lessonText: {
+    fontSize: 14,
   },
 });
 
--- Calendar/screens/UploadScreen.js
+++ Calendar/screens/UploadScreen.js
@@ -1,72 +1,103 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Header from '../components/Header';
 
 const { height } = Dimensions.get('window');
+
 const courses = {
-  '1 курс': {
-    '110 ,111 ,112': 'https://drive.google.com/file/d/1NygtjT3MhXnb_Hf234y5oRle_bIvi9pU/view',
-    '115,116': 'https://drive.google.com/file/d/12nA5UV5WUnJLwb-HF3CR1c7WfTSNBNao/view',
-    '118': 'https://drive.google.com/file/d/1CLpXLMua5d7cNjfTlZGwLPxT3mrVC5zG/view',
-    '130,131': 'https://drive.google.com/file/d/1Mfz78MU9s6wruqbRpz9i5RvWtDuoV6x7/view',
-    '132': 'https://drive.google.com/file/d/1cEM9N5AElGUmJoU-15BJ24OVTHef6zV9/view',
-    '135': 'https://drive.google.com/file/d/1HsHAqeIl-uW4949zicxj3UwYrV_IzNSn/view',
-    '136': 'https://drive.google.com/file/d/18wSQGquaVFANXUIUz0K6FZrXMVllzYGN/view',
-    '151': 'https://docs.google.com/spreadsheets/d/1L8EYGfnhAXcpljCwPG628Kz49IUl8HlV/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '161,162': 'https://docs.google.com/spreadsheets/d/1vv_qWIfxEjXfy9hcyeat8l8U0OUS4Hkl/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-  },
-  '2 курс': {
-    '210,211,212,213': 'https://drive.google.com/file/d/1CuP5PWvjkGNeAPdfDkxrQZ_ge2bHWH96/view',
-    '215,216': 'https://drive.google.com/file/d/1e-N9x5JxoiM4tcvFalcgAADuocsSq0kU/view',
-    '231,233': 'https://drive.google.com/file/d/1bJO6nWpJPtNVYoACPoCrsgyFT_1R8SbU/view',
-    '232': 'https://drive.google.com/file/d/1gpq3X677-iieZ2n3IQ6-t2MlOAuWCdZm/view',
-    '235,236,237': 'https://drive.google.com/file/d/1IIhpiM-3PKmNV3d0FHcrlTEkiKuqF2uY/view',
-    '251': 'https://docs.google.com/spreadsheets/d/1r4qIP-iaFMDM-09OSkGr7JhaSXHFeTqz/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '261': 'https://drive.google.com/file/d/1smrHVixVBZEels6Jgvjac_-GlQnKS7sE/view?usp=sharing',
-  },
-  '3 курс': {
-    '310,311,312,313': 'https://drive.google.com/file/d/1YAoG5VhRaxJD7GxaBlG8QMZY5v_8QVz1/view',
-    '315,316,317': 'https://docs.google.com/spreadsheets/d/1pGXKGo52KYgV2m-m7db838E6ybpE3cOm/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '331,333,334': 'https://docs.google.com/spreadsheets/d/1AU7T35CcZjCbI_6tuS0YmzqmSOYhunfe/edit?usp=sharing&rtpof=true&sd=true',
-    '332': 'https://docs.google.com/spreadsheets/d/16chpi_-you6c6t_PallQs7JvPxNNnp6s/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '335,336,337,338': 'https://docs.google.com/spreadsheets/d/1chLLGGqMokyhQEecvDiLyFjn011Xeb1N/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '351': 'https://docs.google.com/spreadsheets/d/12sejtUDvdQmVXXwFBUW26Dw84yLbugUC/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '361,362': 'https://docs.google.com/spreadsheets/d/19uVuc4AZmebOfrswticuObG7MtaYgBq4/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-  },
-  '4 курс': {
-    '411,413,414': 'https://docs.google.com/spreadsheets/d/1vdXKbO6Gv25d5oW3xTCS3pSCaRTJnmwq/edit?usp=sharing&rtpof=true&sd=true',
-    '415,415': 'https://docs.google.com/spreadsheets/d/11Lhl4me78tT8IP8jwOHF3NB7p1n22hk9/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '431,433,434': 'https://docs.google.com/spreadsheets/d/1zsf2EJDOZZO31y2fDIdaF4SIQnGg_aU-/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '432': 'https://docs.google.com/spreadsheets/d/147Wx0s23_WLTffaubOeN5zzR5V2rDD_u/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '435,436,437': 'https://docs.google.com/spreadsheets/d/1tuZaB-UYFDgNLaqWeAR0BLC4Xim8c3kZ/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '451': 'https://drive.google.com/file/d/1fHOOU_onF0FGNGHLwSDcJlcOW4EjtQwy/view',
-    '461': 'https://docs.google.com/spreadsheets/d/1TPLB77WK_BDVY2J6SFNBTvDMhzH6am_b/edit#gid=1238846972',
-  },
-  '5 курс': {
-    '511': 'https://drive.google.com/file/d/1GKMugF7BVfCXqCfSyqQ4BeC9DFhZorxm/view',
-    '515': 'https://docs.google.com/spreadsheets/d/1HbXiKY7qNOIpUtRAu7CKGT_X_LiGj8t6/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '531': 'https://drive.google.com/file/d/1vbTQsBPI9Rvlg-SIW9Yj0a7KVY3TtNOa/view',
-    '532': 'https://docs.google.com/spreadsheets/d/1lDsV03yk8KKVIYWUSMikW_AGD3bXQClI/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '535,536': 'https://drive.google.com/file/d/1NE2cwZTvkln1OWF0MJRCcNlLrSFUbTaU/view',
-    '551': 'https://docs.google.com/spreadsheets/d/1TOzn8nyvFwi0lHwxnMbTehgwiZgwQ8_g/edit?usp=sharing&ouid=110982410831226660809&rtpof=true&sd=true',
-    '561': 'https://drive.google.com/file/d/1-fqRlFBjLe2wmz_4-a2vnZuZ_KAHtftD/view',
-  }
+  '1 курс': ['110', '111', '112', '115', '116', '118', '130', '131', '132', '135', '136','151','161','162'],
+  '2 курс': ['210', '211', '212', '213', '215', '216', '231', '233', '232', '235', '236', '237', '251', '261'],
+  '3 курс': ['310', '311', '312', '313', '315', '316', '317', '331', '333', '334', '332', '335', '336', '337', '338', '351', '361', '362'],
+  '4 курс': ['411', '413', '414', '415', '416', '431', '433', '434', '432', '435', '436', '437', '451', '461'],
+  '5 курс': ['511', '515', '531', '532', '535', '536', '551', '561']
 };
+
+const schedules = {
+  '110': require('../redux/Shedules/110.json'),
+  '111': require('../redux/Shedules/111.json'),
+  '112': require('../redux/Shedules/112.json'),
+  '115': require('../redux/Shedules/115.json'),
+  '116': require('../redux/Shedules/116.json'),
+  '118': require('../redux/Shedules/118.json'),
+  '130': require('../redux/Shedules/130.json'),
+  '131': require('../redux/Shedules/131.json'),
+  '132': require('../redux/Shedules/132.json'),
+  '135': require('../redux/Shedules/135.json'),
+  '151': require('../redux/Shedules/151.json'),
+  '161': require('../redux/Shedules/161.json'),
+  '162': require('../redux/Shedules/162.json'),
+  '136': require('../redux/Shedules/136.json'),
+  '210': require('../redux/Shedules/210.json'),
+  '211': require('../redux/Shedules/211.json'),
+  '212': require('../redux/Shedules/212.json'),
+  '213': require('../redux/Shedules/213.json'),
+  '215': require('../redux/Shedules/215.json'),
+  '216': require('../redux/Shedules/216.json'),
+  '231': require('../redux/Shedules/231.json'),
+  '233': require('../redux/Shedules/233.json'),
+  '232': require('../redux/Shedules/232.json'),
+  '235': require('../redux/Shedules/235.json'),
+  '236': require('../redux/Shedules/236.json'),
+  '237': require('../redux/Shedules/237.json'),
+  '251': require('../redux/Shedules/251.json'),
+  '261': require('../redux/Shedules/261.json'),
+  '310': require('../redux/Shedules/310.json'),
+  '311': require('../redux/Shedules/311.json'),
+  '312': require('../redux/Shedules/312.json'),
+  '313': require('../redux/Shedules/313.json'),
+  '315': require('../redux/Shedules/315.json'),
+  '316': require('../redux/Shedules/316.json'),
+  '317': require('../redux/Shedules/317.json'),
+  '331': require('../redux/Shedules/331.json'),
+  '333': require('../redux/Shedules/333.json'),
+  '334': require('../redux/Shedules/334.json'),
+  '332': require('../redux/Shedules/332.json'),
+  '335': require('../redux/Shedules/335.json'),
+  '336': require('../redux/Shedules/336.json'),
+  '337': require('../redux/Shedules/337.json'),
+  '338': require('../redux/Shedules/338.json'),
+  '351': require('../redux/Shedules/351.json'),
+  '361': require('../redux/Shedules/361.json'),
+  '362': require('../redux/Shedules/362.json'),
+  '411': require('../redux/Shedules/411.json'),
+  '413': require('../redux/Shedules/413.json'),
+  '414': require('../redux/Shedules/414.json'),
+  '415': require('../redux/Shedules/415.json'),
+  '416': require('../redux/Shedules/416.json'),
+  '431': require('../redux/Shedules/431.json'),
+  '433': require('../redux/Shedules/433.json'),
+  '434': require('../redux/Shedules/434.json'),
+  '432': require('../redux/Shedules/432.json'),
+  '435': require('../redux/Shedules/435.json'),
+  '436': require('../redux/Shedules/436.json'),
+  '437': require('../redux/Shedules/437.json'),
+  '451': require('../redux/Shedules/451.json'),
+  '461': require('../redux/Shedules/461.json'),
+  '511': require('../redux/Shedules/511.json'),
+  '515': require('../redux/Shedules/515.json'),
+  '531': require('../redux/Shedules/531.json'),
+  '532': require('../redux/Shedules/532.json'),
+  '535': require('../redux/Shedules/535.json'),
+  '536': require('../redux/Shedules/536.json'),
+  '551': require('../redux/Shedules/551.json'),
+  '561': require('../redux/Shedules/561.json')
+};
+
 
 const ScheduleApp = () => {
   const [selectedCourse, setSelectedCourse] = useState('');
-  const [selectedSpecialty, setSelectedSpecialty] = useState('');
+  const [selectedGroup, setSelectedGroup] = useState('');
   const navigation = useNavigation();
 
   useEffect(() => {
     const loadData = async () => {
       try {
         const course = await AsyncStorage.getItem('@selectedCourse');
-        const specialty = await AsyncStorage.getItem('@selectedSpecialty');
+        const group = await AsyncStorage.getItem('@selectedGroup');
         if (course) setSelectedCourse(course);
-        if (specialty) setSelectedSpecialty(specialty);
+        if (group) setSelectedGroup(group);
       } catch (e) {
         console.error('Error loading data:', e);
       }
@@ -74,10 +105,10 @@ const ScheduleApp = () => {
     loadData();
   }, []);
 
-  const saveSelection = async (course, specialty) => {
+  const saveSelection = async (course, group) => {
     try {
       if (course !== null) await AsyncStorage.setItem('@selectedCourse', course);
-      if (specialty !== null) await AsyncStorage.setItem('@selectedSpecialty', specialty);
+      if (group !== null) await AsyncStorage.setItem('@selectedGroup', group);
     } catch (e) {
       console.error('Error saving data:', e);
     }
@@ -85,21 +116,19 @@ const ScheduleApp = () => {
 
   const handleCourseChange = (course) => {
     setSelectedCourse(course);
-    setSelectedSpecialty(''); // Clear the specialty selection
+    setSelectedGroup(''); // Clear the group selection
     saveSelection(course, '');
   };
 
-  const handleSpecialtyChange = (specialty) => {
-    setSelectedSpecialty(specialty);
-    saveSelection(null, specialty);
-    const selectedLink = courses[selectedCourse][specialty];
-    const injectedJavaScript = `
-      var meta = document.createElement('meta'); 
-      meta.name = 'viewport'; 
-      meta.content = 'width=device-width, initial-scale=0.47, maximum-scale=0.7, user-scalable=yes'; 
-      document.getElementsByTagName('head')[0].appendChild(meta);
-    `;
-    navigation.navigate('Schedule', { selectedLink, injectedJavaScript });
+  const handleGroupChange = (group) => {
+    setSelectedGroup(group);
+    saveSelection(null, group);
+    const scheduleData = schedules[group];
+    if (scheduleData) {
+      navigation.navigate('Schedule', { group, scheduleData });
+    } else {
+      console.error('Error fetching schedule data: Schedule not found');
+    }
   };
 
   const renderCourseButtons = () => {
@@ -110,11 +139,11 @@ const ScheduleApp = () => {
     ));
   };
 
-  const renderSpecialtyButtons = () => {
+  const renderGroupButtons = () => {
     if (!selectedCourse) return null;
-    return Object.keys(courses[selectedCourse]).map((specialty) => (
-      <TouchableOpacity key={specialty} style={styles.button} onPress={() => handleSpecialtyChange(specialty)}>
-        <Text style={styles.buttonText}>{specialty}</Text>
+    return courses[selectedCourse].map((group) => (
+      <TouchableOpacity key={group} style={styles.button} onPress={() => handleGroupChange(group)}>
+        <Text style={styles.buttonText}>{group}</Text>
       </TouchableOpacity>
     ));
   };
@@ -122,7 +151,7 @@ const ScheduleApp = () => {
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
-        <View style={styles.contentContainer}>
+        <ScrollView style={styles.contentContainer}>
           <Text style={styles.title}>Виберіть курс:</Text>
           <View style={styles.buttonContainer}>
             {renderCourseButtons()}
@@ -131,11 +160,11 @@ const ScheduleApp = () => {
             <>
               <Text style={styles.title}>Виберіть групу:</Text>
               <View style={styles.buttonContainer}>
-                {renderSpecialtyButtons()}
+                {renderGroupButtons()}
               </View>
             </>
           ) : null}
-        </View>
+        </ScrollView>
       </SafeAreaView>
       <Header style={styles.header} />
     </View>
@@ -154,7 +183,6 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 1,
-    justifyContent: 'center',
     paddingHorizontal: 20,
   },
   buttonContainer: {
